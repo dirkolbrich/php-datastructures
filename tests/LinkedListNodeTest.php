96a7de7a79b8923fbb5018e1c1eb3c8460bec567
Dirk Olbrich <mail@dirkolbrich.de>
2017-04-21 22:48:38 +0200

    test phpunit setup

--- tests/LinkedListNodeTest.php
+++ tests/LinkedListNodeTest.php
@@ -1,9 +1,9 @@
 <?php
 declare(strict_types=1);
 
-namespace DataStructures\Tests;
+//namespace DataStructures\Tests;
 
-use DataStructures\LinkedListNode;
+//use DataStructures\LinkedListNode;
 use PHPUnit\Framework\TestCase;
 
 class LinkedListNodeTest extends TestCase
